71896db38e2e348545ded2a9ff5594d33f827dff
李法金 <wb.lifajin@mesg.corp.netease.com>
2020-01-15 20:17:48 +0800

    1.日志添加颜色

--- src/main/java/com/netease/example/controllers/ExampleController.java
+++ src/main/java/com/netease/example/controllers/ExampleController.java
@@ -1,15 +1,18 @@
 package com.netease.example.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class ExampleController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String index(@RequestParam String name) {
+        log.info("hello "+name+"，this is first messge");
         return "hello "+name+"，this is first messge";
     }
 }
