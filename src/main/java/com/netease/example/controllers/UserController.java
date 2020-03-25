d55fd1e5d283998b26214469f5968dee1f5d9081
李法金 <wb.lifajin@mesg.corp.netease.com>
2020-03-25 15:58:46 +0800

    1.优化mongo链接,改写接口为mongoTemplate实现

--- src/main/java/com/netease/example/controllers/UserController.java
+++ src/main/java/com/netease/example/controllers/UserController.java
@@ -5,18 +5,16 @@ import com.netease.example.models.MyResponseContent;
 import com.netease.example.models.repositories.UserRepository;
 import com.netease.example.models.user.User;
 import io.swagger.annotations.ApiOperation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private UserRepository userRepository;
@@ -27,9 +25,9 @@ public class UserController {
     @ApiOperation(value = "通过id获取用户信息", notes="返回用户信息")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public MyResponseContent getUserById(@PathVariable String id) {
-        User user = userRepository.findById(id).get();
+        User user = userRepository.findById(id);
         com.netease.example.domain.User user1 = userDao.selectByPrimaryKey(Integer.parseInt(id));
-        logger.info("获取成功");
+        log.info("获取成功");
         return new MyResponseContent<>(true, user, "获取成功");
     }
 
@@ -37,7 +35,7 @@ public class UserController {
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public MyResponseContent addUser(@RequestBody User user) {
         userRepository.insert(user);
-        logger.info("创建成功");
+        log.info("创建成功");
         return new MyResponseContent<>(true, user, "创建成功");
     }
 
@@ -45,7 +43,7 @@ public class UserController {
     @RequestMapping(value = "/allUser", method = RequestMethod.GET)
     public MyResponseContent getAllUser() {
         List<User> users = userRepository.findAll();
-        logger.info("获取所有用户成功");
+        log.info("获取所有用户成功");
         return new MyResponseContent<>(true, users, "获取所有用户成功");
     }
 
@@ -53,7 +51,7 @@ public class UserController {
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public MyResponseContent deleteUser(@PathVariable String id) {
         userRepository.deleteById(id);
-        logger.info("删除成功");
+        log.info("删除成功");
         return new MyResponseContent<>(true, null, "删除成功");
     }
 
@@ -61,7 +59,7 @@ public class UserController {
     @RequestMapping(value = "/{id}", method = RequestMethod.PATCH)
     public MyResponseContent updateUser(@RequestBody User user) {
         userRepository.save(user);
-        logger.info("更新成功");
+        log.info("更新成功");
         return new MyResponseContent<>(true, user, "更新成功");
     }
 
--- src/main/java/com/netease/example/models/repositories/UserRepository.java
+++ src/main/java/com/netease/example/models/repositories/UserRepository.java
@@ -1,8 +1,42 @@
 package com.netease.example.models.repositories;
 
+import com.mongodb.client.result.DeleteResult;
 import com.netease.example.models.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByName(String name);
+import java.util.List;
+
+public class UserRepository {
+    @Autowired
+    @Qualifier(value = "primaryReviewMongoTemplate")
+    private MongoTemplate mongoTemplate;
+
+    public User findById(String id) {
+        User user = mongoTemplate.findById(id, User.class);
+        return user;
+    }
+
+    public void insert(User user) {
+        mongoTemplate.insert(user);
+    }
+
+    public User save(User user) {
+        user = mongoTemplate.save(user);
+        return user;
+    }
+
+    public long deleteById(String id) {
+        Query query = new Query(Criteria.where("_id").is(id));
+        DeleteResult result = mongoTemplate.remove(query, User.class);
+        return result.getDeletedCount();
+    }
+
+    public List<User> findAll() {
+        return mongoTemplate.findAll(User.class);
+    }
 }
